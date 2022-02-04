aadb8ea97a425fe9e92bb5999edb82715bb89544
saharlevy <sahar.levy@tonkean.com>
2022-02-04 15:10:07 +0200

    import useSum to export from index file

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import useSwitcher from "./src/useSwitcher/useSwitcher";
 import useRandom from "./src/useRandom/useRandom";
 import usePrefix from "./src/usePrefix/usePrefix";
+import useSum from "./src/useSum/useSum";
 
-export {usePrefix, useRandom, useSwitcher};
+export {usePrefix, useRandom, useSwitcher, useSum};
