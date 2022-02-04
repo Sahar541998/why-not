2edc2aa7d67ce6ed78c8867b6a920710f0078ddf
saharlevy <sahar.levy@tonkean.com>
2022-02-04 15:02:49 +0200

    import useSwitcher,useRandom,usePrefix to export them from index file

---
+++ index.ts
@@ -0,0 +1,5 @@
+import useSwitcher from "./src/useSwitcher/useSwitcher";
+import useRandom from "./src/useRandom/useRandom";
+import usePrefix from "./src/usePrefix/usePrefix";
+
+export {usePrefix, useRandom, useSwitcher};
